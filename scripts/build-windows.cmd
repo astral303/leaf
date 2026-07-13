049d61ea55f5d58ab07ee11930f361b346da918c
Oksana Rekutina <github@36hz.com>
2026-07-12 20:03:30 -0400

    build: forward Cargo arguments through Windows wrapper

--- scripts/build-windows.cmd
+++ scripts/build-windows.cmd
@@ -1,5 +1,5 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=x64 -host_arch=x64 >nul
+call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=x64 -host_arch=x64 -no_logo
 if errorlevel 1 exit /b %errorlevel%
 
-cargo build --release
+cargo %*
